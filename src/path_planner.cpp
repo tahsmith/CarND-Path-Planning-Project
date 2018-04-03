aaae3a09dea98b826b55f908e17dbaca403628a8
tahsmith <tahs88@gmail.com>
2018-04-03 20:44:31 +1000

    Follow lane exactly

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -6,7 +6,6 @@
 #include <cmath>
 
 #include "path_planner.hpp"
-#include "map_data.hpp"
 #include "utilities.hpp"
 
 PathPlanner::PathPlanner(MapData mapData) :
@@ -16,44 +15,16 @@ PathPlanner::PathPlanner(MapData mapData) :
 Path PathPlanner::PlanPath()
 {
     Path path{};
-    double pos_x;
-    double pos_y;
-    double angle;
-    int path_size = previousPath.x.size();
 
-    for (int i = 0; i < path_size; i++)
-    {
-        path.x.push_back(previousPath.x[i]);
-        path.y.push_back(previousPath.y[i]);
-    }
-
-    if (path_size == 0)
-    {
-        pos_x = state.x;
-        pos_y = state.y;
-        angle = deg2rad(state.yaw);
-    }
-    else
-    {
-        pos_x = previousPath.x[path_size - 1];
-        pos_y = previousPath.y[path_size - 1];
-
-        double pos_x2 = previousPath.x[path_size - 2];
-        double pos_y2 = previousPath.y[path_size - 2];
-        angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
-    }
-
+    double d = 6;
     double dist_inc = 0.5;
-    for (int i = 0; i < 50 - path_size; i++)
+    for (int i = 0; i < 50; i++)
     {
-        path.x
-            .push_back(
-                pos_x + (dist_inc) * cos(angle + (i + 1) * (pi() / 100)));
-        path.y
-            .push_back(
-                pos_y + (dist_inc) * sin(angle + (i + 1) * (pi() / 100)));
-        pos_x += (dist_inc) * cos(angle + (i + 1) * (pi() / 100));
-        pos_y += (dist_inc) * sin(angle + (i + 1) * (pi() / 100));
+        double s = state.s + (i + 1) * dist_inc;
+        auto xy = getXY(s, d, this->mapData.waypoints_s,
+                        this->mapData.waypoints_x, this->mapData.waypoints_y);
+        path.x.push_back(xy[0]);
+        path.y.push_back(xy[1]);
     }
     return path;
 }
