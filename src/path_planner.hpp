59f29179a3cbad68b2cc9036c223f0ade1460c8d
tahsmith <tahs88@gmail.com>
2018-04-22 20:14:42 +1000

    Clean up

--- src/path_planner.hpp
+++ src/path_planner.hpp
@@ -48,15 +48,42 @@ class PathPlanner
 {
 public:
     PathPlanner(double dt, MapData mapData);
-
     void UpdateLocalisation(VehicleState state);
-
     void UpdateHistory(Path previousPath);
-
     void UpdateSensorFusion(SensorFusionData);
-
     Path PlanPath();
-
+    Plan GeneratePlanForState(uint8_t state) const;
+    Path
+    GenerateTrajectoryFromCurrent(double t_final, double s_final,
+                                  double d_final,
+                                  double speed_final) const;
+    typedef std::map<std::string, std::tuple<double, double, double> > CostDebugInfo;
+    double CostForTrajectory(const Plan& plan, CostDebugInfo& debug_info) const;
+    void PrintDebugInfo(const CostDebugInfo&);
+    size_t FindCarToFollow(long lane) const;
+    double SafeSpeedForLane(long lane) const;
+    void UpdateCarPaths();
+    double CarPotential(double x, double y,
+                        double vx, double vy,
+                        double car_x, double car_y,
+                        double car_vx, double car_vy) const;
+    double CarAvoidanceCostPerCar(const Path& path, size_t i) const;
+    double CarAvoidanceCost(const Path& path) const;
+    double SoftCarPotential(double x, double y,
+                            double vx, double vy,
+                            double car_x, double car_y,
+                            double car_vx, double car_vy) const;
+    double SoftCarAvoidanceCostPerCar(const Path& path, size_t i) const;
+    double SoftCarAvoidanceCost(const Path& path) const;
+    Path GenerateTrajectory(double t_final, double s_final, double d_final,
+                            double speed_final, double x_initial, double y_initial,
+                            double vx_initial, double vy_initial, double ax_initial,
+                            double ay_initial) const;
+    Path InterpolatePath(double t_final, double x_initial, double x_final,
+                             double y_initial, double y_final, double vx_initial,
+                             double vx_final, double vy_initial, double vy_final,
+                             double ax_initial, double ax_final, double ay_initial,
+                             double ay_final) const;
 
     const double hard_speed_limit = 22.35;
     const double speed_limit = hard_speed_limit * 0.95;  //  22.35m s^-1 ~= 50 miles / hr
@@ -71,51 +98,10 @@ public:
     Plan current_plan;
     long lane_actual;
     SensorFusionData sensor_fusion_data;
+
     std::vector<Path> car_paths;
 
     Path previous_path;
-
-    Path
-    GenerateTrajectoryFromCurrent(double t_final, double s_final,
-                                  double d_final,
-                                  double speed_final) const;
-    Plan GeneratePlanForState(uint8_t state) const;
-
-    typedef std::map<std::string, std::tuple<double, double, double> > CostDebugInfo;
-    double CostForTrajectory(const Plan& plan, CostDebugInfo& debug_info) const;
-    void PrintDebugInfo(const CostDebugInfo&);
-
-    size_t FindCarToFollow(long lane) const;
-
-    double SafeSpeedForLane(long lane) const;
-
-    void UpdateCarPaths();
-
-    double CarPotential(double x, double y,
-                        double vx, double vy,
-                        double car_x, double car_y,
-                        double car_vx, double car_vy) const;
-    double CarAvoidanceCostPerCar(const Path& path, size_t i) const;
-    double CarAvoidanceCost(const Path& path) const;
-
-    double SoftCarPotential(double x, double y,
-                            double vx, double vy,
-                            double car_x, double car_y,
-                            double car_vx, double car_vy) const;
-    double SoftCarAvoidanceCostPerCar(const Path& path, size_t i) const;
-    double SoftCarAvoidanceCost(const Path& path) const;
-
-
-    Path GenerateTrajectory(double t_final, double s_final, double d_final,
-                            double speed_final, double x_initial, double y_initial,
-                            double vx_initial, double vy_initial, double ax_initial,
-                            double ay_initial) const;
-
-    Path InterpolatePath(double t_final, double x_initial, double x_final,
-                             double y_initial, double y_final, double vx_initial,
-                             double vx_final, double vy_initial, double vy_final,
-                             double ax_initial, double ax_final, double ay_initial,
-                             double ay_final) const;
 };
 
 
